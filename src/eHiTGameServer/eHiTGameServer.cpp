8c83166bcd94139a619aef89983a76556626dcc2
Halil Ibrahim TOMAK <tomaklar@hotmail.com>
2018-09-19 08:50:39 +0300

    Re orginesed
    Re orginised

---
+++ src/eHiTGameServer/eHiTGameServer.cpp
@@ -0,0 +1,6 @@
+//Veritabanli kullanici sistemiyle program acilacak
+	//Veri tabani baglanti yardimcisi
+	//Veri tabani kullanici olusturma ve silme ve yetkiler
+	//Veri tabani uye giris cikisi yonetimi
+//Veri tabanindan alinan bilgilere gore portlar dinlencek
+//Veri tabanindan alinan bilgilere gore islemler yapilcak
